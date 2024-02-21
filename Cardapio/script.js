76e4240003a622ff9f9d0f003f238ca0c7d8a954
LauraMariana27 <documentoslams@gmail.com>
2024-02-21 11:41:29 -0300

    Aula21/2

---
+++ Cardapio/script.js
@@ -0,0 +1,8 @@
+
+function muda_cardapio(n) {
+    var cardapios = document.querySelectorAll('.cardapio');
+    cardapios.forEach(cardapio => {
+        cardapio.classList.add('hidden');
+    });
+    cardapios[n].classList.remove('hidden');
+}
